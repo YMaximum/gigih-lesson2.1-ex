3c40154c851587e86d567979896e2707d0cfbdde
Yassar <yassarnaufal@gmail.com>
2023-07-10 20:37:24 +0700

    adding semicolon at the end for some

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.get('/about', (req, res) => {
     res.send('This is the about page');
@@ -16,4 +16,4 @@ app.use((req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
